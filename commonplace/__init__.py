2e20fba1f355add1a99de4a5320480ed41096224
David Jiménez <djimenez81@gmail.com>
2025-11-08 17:29:34 -0600

    Started playing with the __init__.py files, but I am getting import errors. I will try to fix it later.

--- commonplace/__init__.py
+++ commonplace/__init__.py
@@ -1,1 +1,11 @@
-# Initialization script for: 
+# Initialization script for: commonplace package.
+
+
+# Imports
+from .management.parser import Parser, NoteDatabase
+from .core.core         import BasicNote, NoteCollection, NoteModule
+from .utils.basic_utils import _generate_code
+
+
+# Metadata
+__version__ = '0.1.0' # I am starting to play
--- commonplace/management/__init__.py
+++ commonplace/management/__init__.py
@@ -1,1 +1,5 @@
-# Initialization script for: 
+# Initialization script for: management submodule
+
+
+# Metadata
+__version__ = '0.1.0' # I am starting to play
