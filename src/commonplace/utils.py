a7ebc1732ea6e80d3ff1d09b22df6ff7831d81c8
David Jimenez <djimenez81@gmail.com>
2026-04-02 16:30:25 -0600

    Added the 'is_valid_uuid' method.

--- src/commonplace/utils.py
+++ src/commonplace/utils.py
@@ -11,6 +11,7 @@ __version__ = "0.1.0"
 
 
 # Strandard library imports
+import uuid
 
 
 # Third party imports
@@ -21,10 +22,37 @@ import yaml
 
 
 
-def return_yaml_list(file_path):
+def return_yaml_list(file_path: str) -> list[dict]:
+    """Returns a list of YAML documents (dictinaries) contained in a file.
+
+    Attributes:
+      - file_path: The path of the YAML file.
+
+    Returns:
+      - list of dictionaries
+    """
     yaml_list = list()
     with open(file_path, 'r') as file:
         data = yaml.safe_load_all(file)
         for doc in data:
             yaml_list.append(doc)
     return yaml_list
+
+
+
+def is_valid_uuid(possible_uuid: str) -> bool:
+    """Checks if a given string is a UUID or not.
+
+    Attributes:
+      - val: String that could be.
+
+    Returns:
+      - True if string is a valid UUID, false otherwise.
+    """
+    try:
+        # Attempts to parse the string as a UUID
+        uuid.UUID(str(possible_uuid))
+        return True
+    except ValueError:
+        # Raised if the string is not a valid 32-character hex string
+        return False
