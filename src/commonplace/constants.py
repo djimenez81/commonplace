54ca377275dcd621b29433666a2beaa351603ba2
David Jimenez <djimenez81@gmail.com>
2026-04-02 16:27:21 -0600

    Initial Commit. This is a constants module, started with about a dozen global constants that are needed.

---
+++ src/commonplace/constants.py
@@ -0,0 +1,27 @@
+"""This module contains the global constants for Commonplace.
+"""
+
+
+__author__  = "David Jiménez"
+__email__   = "djimenez81@gmail.com"
+__status__  = "Initial implementation"
+__version__ = "0.1.0"
+
+
+USER = "user"
+SYSTEM = "system"
+
+NEW_FIELD_TYPE = "new_field_type"
+NEW_NOTE_TYPE = "new_note_type"
+NEW_COLLECTION_TYPE = "new_collection_type"
+
+FIELD_NAME = "field_name"
+FIELD_TYPE = "field_type"
+
+REQUIRED = "required"
+ASSIGNED = "assigned"
+OVERWRITE = "overwrite"
+REASSIGN = "reassign"
+DEF_ORIGIN = "definition_origin"
+
+NIL_UUID = "00000000-0000-0000-0000-000000000000"
