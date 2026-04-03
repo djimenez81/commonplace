900ffb7cbf2ee3d5158473c0a53748c117f4c79d
David Jimenez <djimenez81@gmail.com>
2026-04-03 16:49:25 -0600

    Added constants related to loading Note Definitions.

--- src/commonplace/constants.py
+++ src/commonplace/constants.py
@@ -18,10 +18,17 @@ NEW_COLLECTION_TYPE = "new_collection_type"
 FIELD_NAME = "field_name"
 FIELD_TYPE = "field_type"
 
+NOTE_TYPE = "note_type"
+
 REQUIRED = "required"
 ASSIGNED = "assigned"
 OVERWRITE = "overwrite"
 REASSIGN = "reassign"
 DEF_ORIGIN = "definition_origin"
+
+EXTENDS = "extends"
+META_FIELDS = "metadata_fields"
+ADD_META = "additional_metadata"
+BODY_SPEC = "body_specification"
 
 NIL_UUID = "00000000-0000-0000-0000-000000000000"
