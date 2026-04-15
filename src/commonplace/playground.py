85914f7ddf6f2017671d2b86b262cebbb7ef8fdb
David Jimenez <djimenez81@gmail.com>
2026-04-14 21:00:38 -0600

    Added some code I ran manualy to test the loading of definitions.

--- src/commonplace/playground.py
+++ src/commonplace/playground.py
@@ -1,11 +1,22 @@
-"""This module contains code that is being played around
+"""Test during development.
 
-This file is temporary and only contains code I am trying to understand how
-third party libraries behave, or just dumb stuff I am trying, that if it works,
-it will be relocated elsewhere.
+This file simply contains snippets of code that I run manually while
+developing other parts of the code. Not proper tests, but mini-tests to
+ensure that I am understanding myself what I am writing.
 
-Don't be surprised if this file disappear later on.
+Also, it might contain pieces of code to understand standard or third
+party libraries that I am using for the first time.
 
+This file might be deleted at the end.
 """
 
-import yaml
+# Testing that loading new definitions works.
+import utils
+import config
+
+file_path = "../../config/definitions.yaml"
+definitions = utils.return_yaml_list(file_path)
+con_man = config.TypeConfigurationManager()
+con_man.load_definitions(definitions)
+
+# So far seems to be doing what I want it to do.
